9eb8d4cc46a112fe6df452dd0d6e6106fc43ba80
Eric Coffin <eric.coffin@gmail.com>
2020-01-02 18:26:06 -0400

    Generate CSV for tp_runner

--- src/jitbuilder/recursive-fib/tp_runner.py
+++ src/jitbuilder/recursive-fib/tp_runner.py
@@ -1,13 +1,16 @@
+import csv
 import glob
 import math
 import os
+import statistics
 import subprocess
+import sys
 
 print("Clearing old results")
 for f in glob.glob("./build/tp_results/*"):
     os.remove(f)
 
-count = 11
+count = 100
 
 max_time_sec = 0
 max_time_ns = 10000000
@@ -22,23 +25,56 @@ for t in range(1, count):
     
 print("Finished running tests")
 
-# group the method invocations at time t into buckets of size ns_bucket_size
-# note trailing integer division '//'
-num_buckets = ( (max_time_sec * 1000000000 + max_time_ns) // ns_bucket_size )
-
-# create array of size num_buckets initialized to zero
-result = [0] * ( num_buckets + 1 )
-
-print("bucket_size=" + str(ns_bucket_size) + ", num_buckets=" + str(num_buckets))
+results = {}
 
 # discard first 10 runs
 for t in range(10, count):
+    run_index = t - 9
     with open(out_path + str(t).zfill(3) + ".txt") as f:
         print(out_path + str(t).zfill(3) + ".txt")
         for i in f:
-            print(i)
-            print(int(i) // ns_bucket_size)
-            result[int(i) // ns_bucket_size] += 1       
+            bucket = ( int(i) // ns_bucket_size ) * ns_bucket_size
+            if bucket in results:
+                if run_index in results[bucket]:
+                    results[bucket][run_index] += 1
+                else:
+                    results[bucket][run_index] = 1
+            else:
+                results[bucket] = { run_index : 1 }
 
-print(result)
+# printing results 
 
+print("bucket_size={}, num_buckets={}".format(ns_bucket_size, len(results)))
+
+last_bucket = list(results.keys())[-1]
+
+# print out csv 
+# last_bucket_1 is a hack to get inclusive range
+table = []
+for bucket in range(0, last_bucket + 1, ns_bucket_size):
+    data = [bucket]
+    for t in range(10, count):
+        run_index = t - 9
+        if bucket in results:
+            if run_index in results[bucket]:
+                data.append(results[bucket][run_index])
+            else:
+                data.append(0)
+        else:
+            data.append(0)
+    # each row contains bucket_index, avg, std_deviation, confidence
+    row = [data[0]]
+
+    avg = sum(data[1:]) / ( len(data) - 1 )
+    row.append(avg)
+    
+    std = statistics.stdev(data[1:])
+    row.append(std)
+    
+    confidence = std / math.sqrt(len(data) - 1)
+    row.append(confidence)
+
+    table.append(row)
+
+writer = csv.writer(sys.stdout)
+writer.writerows(table)
