4cc90200628a57c66ea7aafab5c76a4e86291487
Eric Coffin <eric.coffin@gmail.com>
2020-01-07 16:37:13 -0400

    Updated benches

--- src/run-benches.sh
+++ src/run-benches.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 declare -a frameworks=("llvm" 
-                      #"jitbuilder"
-                      #"native"
+                      "jitbuilder"
+                      "native"
                      )
 
 declare -a tasks=("increment"
-                 #"recursive-fib"
-                 #"iterative-fib"
+                 "recursive-fib"
+                 "iterative-fib"
                 )
 
 cd "$(dirname "$0")"
@@ -15,17 +15,11 @@ pwd=$PWD
 
 for framework in "${frameworks[@]}"
 do
-    echo "Running $f benchmark..."
     for task in "${tasks[@]}"
     do
+        echo "Running $framework/$task benchmarks..."
         cd "$pwd/$framework/$task/"
         make run-bench > bench-results.txt
     done
     cd ..
 done
-
-
-#cd llvm/increment
-
-#make clean
-#make run-bench > bench-results.txt
