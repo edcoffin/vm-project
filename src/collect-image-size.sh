c956aec8a11e6c5ada53218662a9f3eee4f49f5c
Eric Coffin <eric.coffin@gmail.com>
2020-01-07 17:01:55 -0400

    Collected image size

--- src/collect-image-size.sh
+++ src/collect-image-size.sh
@@ -9,18 +9,21 @@ declare -a tasks=("increment"
                  "recursive-fib"
                  "iterative-fib"
                 )
-
 cd "$(dirname "$0")"
 pwd=$PWD
+report_file="$pwd/../report/stats/image-sizes.txt"
+echo "" > "$report_file" 
 
 for framework in "${frameworks[@]}"
 do
     for task in "${tasks[@]}"
     do
-        echo "Running $framework/$task benchmarks..."
+        echo "Getting $framework/$task image size..."
         cd "$pwd/$framework/$task/"
         make clean
-        make run-bench > bench-results.txt
+        make main
+        echo "$framework/$task" >> "$report_file"
+        du -b build/main | cut -f 1 >> "$report_file"
     done
     cd ..
 done
